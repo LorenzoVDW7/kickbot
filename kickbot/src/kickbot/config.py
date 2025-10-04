8d76e8d0ea5dbe2e633f4a8bdfab9c1749966d6d
Lorenzo <Lvanderwoude@hotmail.nl>
2025-10-04 17:21:15 +0200

    restructures bot to use a client class

--- kickbot/src/kickbot/config.py
+++ kickbot/src/kickbot/config.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv("DISCORD_TOKEN")
+server_id = os.getenv("SERVER_ID")
--- kickbot/src/kickbot/main.py
+++ kickbot/src/kickbot/main.py
@@ -1,22 +1,44 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
+from discord import app_commands
 import logging
-from config import token as client_token
+from config import token as client_token, server_id
+
+class Client(commands.Bot):
+    async def on_ready(self):
+        guild = discord.Object(id=server_id)
+
+        print(f'Logged in as {self.user}')
+
+        try:
+            synced = await self.tree.sync(guild=guild)
+            print(f"synced {len(synced)} commands to guild {guild.id}")
+
+        except Exception as e:
+            print(f"Error syncing commands to guild {guild.id}: {e}")
+            pass
+
 
 handler = logging.FileHandler(filename='kickbot.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+GUILD_ID = discord.Object(id=server_id)
 
-@bot.command()
-async def preview(ctx, description='Shows a preview of users that would be kicked by the "/Kick" command'):
-    lurker_role = discord.utils.get(ctx.guild.roles, name='Lurker')
-    usernames = [member.name for member in ctx.guild.members if lurker_role in member.roles]
-    await ctx.send(usernames)
+client = Client(command_prefix="!", intents=intents)
 
-bot.run(client_token)
+def get_roles(context):
+    return [roles for roles in context.guild.roles]
+
+
+@client.tree.command(name="preview", description='Preview the members that the "/Kick" command would remove',
+                  guild=GUILD_ID)
+async def show_preview(interaction: discord.Interaction, role: str):
+    await interaction.response.send_message(role)
+
+
+client.run(client_token)
 
 
 
